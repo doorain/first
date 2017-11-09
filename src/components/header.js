c82ec635699f8ec410af9f3993c63aaa27e0ec82
Dorian Morgan <doorain@gmail.com>
2017-11-08 23:36:59 -0700

    sass init setup

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render(){
+    return (
+    <header className='header-box'>hello</header>
+    )
+  }
+}
+export default Header
