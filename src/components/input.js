9d6c63ace8a754e903484fcfb1c5b291af047f89
Dorian Morgan <doorain@gmail.com>
2017-11-13 13:35:53 -0700

    set .bind(this)

--- src/components/input.js
+++ src/components/input.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Input extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
-      location: []
+      location: [],
+      locationToDisplay: []
     }
+    this.handleChange = this.handleChange.bind(this);
+    this.getWeather = this.getWeather.bind(this);
   }
+getWeather(){
+  let promise = axios.get('')
+}
+
+handleChange(e){
+  this.setState({
+    location: e.target.value
+  })
+}
 
   render(){
     return(
       <div>
-        <form onSubmit={this.fetchData}>
-            <input
-              placeholder={"City, State"}
-              type="text"
-              value={this.state.location}
-              onChange={this.changeLocation}
-            />
-        </form>
+        <input placeholder={"City, State"} type="text" onChange={this.getWeather}/>
+         <button onClick={this.handleClick}> Search </button>
       </div>
     )
   }
