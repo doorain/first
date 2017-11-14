abfffdd38d642db829530007234bebed38e6aec7
Dorian Morgan <doorain@gmail.com>
2017-11-14 01:07:20 -0700

    parse data

--- src/components/input.js
+++ src/components/input.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import config from './../config.js'
 
 class Input extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      location: [],
-      locationToDisplay: []
+      location: []
+
     }
     this.handleChange = this.handleChange.bind(this);
     this.getWeather = this.getWeather.bind(this);
   }
 getWeather(){
-  let promise = axios.get('')
+
+  let promise = axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=${this.state.location}&APPID=${config.Open_Weather_Map_Key}&units=imperial`)
+  promise.then(res => {
+    this.setState({
+      x:res.data,
+      cityName:res.data.city.name,
+      currentTemp:res.data
+    })
+  })
 }
 
 handleChange(e){
@@ -22,11 +31,17 @@ handleChange(e){
   })
 }
 
+handleClick(){
+
+}
   render(){
+console.log(this.state.x);
     return(
       <div>
-        <input placeholder={"City, State"} type="text" onChange={this.getWeather}/>
-         <button onClick={this.handleClick}> Search </button>
+        <input placeholder={"City, Country"} type="text" onChange={this.handleChange}/>
+         <button onClick={this.getWeather}> Search </button><br/>
+         {this.state.cityName}
+
       </div>
     )
   }
